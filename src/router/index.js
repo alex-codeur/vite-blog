ecd928e518280ad5d77eb1a7f7b9b5b80caef9d2
esakimedia <apachecordovax@gmail.com>
2022-07-10 18:44:57 +0200

    setting up the login and register pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,13 @@ import About from '../views/public/About.vue'
 import Contact from '../views/public/Contact.vue'
 import PostView from '../views/public/PostView.vue'
 
+import Login from '../views/auth/Login.vue'
+import Register from '../views/auth/Register.vue'
+
 import NotFound from '../views/public/NotFound.vue'
 
 import PublicLayout from '../views/public/Layout.vue'
+import AuthLayout from '../views/auth/AuthLayout.vue'
 
 const routes = [
     {
@@ -19,6 +23,16 @@ const routes = [
             { path: '/about', name: 'about', component: About },
             { path: '/contact', name: 'contact', component: Contact },
             { path: '/post', name: 'post', component: PostView }
+        ]
+    },
+    {
+        path: '/auth',
+        redirect: '/login',
+        name: 'auth',
+        component: AuthLayout,
+        children: [
+            { path: '/login', name: 'login', component: Login },
+            { path: '/register', name: 'register', component: Register }
         ]
     },
     {
