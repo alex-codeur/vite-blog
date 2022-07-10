b7a5958ce292e9a6ec37669e36de7f08917ca8e2
esakimedia <apachecordovax@gmail.com>
2022-07-10 10:20:51 +0200

    setting up the Layout page and public pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+import Home from '../views/public/Home.vue'
+import About from '../views/public/About.vue'
+import Contact from '../views/public/Contact.vue'
+
+import NotFound from '../views/public/NotFound.vue'
+
+import PublicLayout from '../views/public/Layout.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'public',
+        component: PublicLayout,
+        children: [
+            { path: '/', name: 'home', component: Home },
+            { path: '/about', name: 'about', component: About },
+            { path: '/contact', name: 'contact', component: Contact }
+        ]
+    },
+    {
+        path: '/:pathMatch(.*)*', component: NotFound
+    }
+];
 
 const router = createRouter({
     history: createWebHistory(),
