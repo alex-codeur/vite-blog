68517c92265d102826428c51820460bb504ac16c
esakimedia <apachecordovax@gmail.com>
2022-07-15 05:48:04 +0200

    setting up the administration part 2

---
+++ src/components/AdminSidebar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AdminSidebar',
+    }
+</script>
--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,10 +1,5 @@
 <template>
     <div>
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
-            </div>
-        </header>
         <main>
             <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 <!-- Replace with your content -->
@@ -18,7 +13,8 @@
 </template>
 
 <script>
+
     export default {
-        name: 'Dashboard'
+        name: 'Dashboard',
     }
 </script>
